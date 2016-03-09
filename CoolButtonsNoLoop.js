e20e63978168a95696cf3d3c43829756d0dc7e7a
Daniel Elebash <danelebash@hotmail.com>
2016-03-08 22:09:17 -0500

    fixed

--- CoolButtonsNoLoop.js
+++ CoolButtonsNoLoop.js
@@ -38,39 +38,57 @@
 //     circle.classList.add("animate");
 // });
 
+//
+// document.addEventListener('click', function (e) {
+//     if (!e.target.matches('.ripple-effect')) {
+//         return
+//     }else {
+//         //check to see if parent has ripple-effect
+//         var clickEl = e.target.closest('.ripple-effect');
+//         // if still null then element does not have ripple-effect
+//         if (!clickEl) {
+//             return
+//         }
+//     }
+//
+//         // we now have valid element with ripple-effect
+//         // determine circle animation diameter
+//         var rect = clickEl.getBoundingClientRect();
+//         var x = event.clientX - rect.left;
+//         var y = event.clientY - rect.top;
+//         var circle = document.createElement("div");
+//
+//         circle.style.top = y + "px";
+//         circle.style.left = x + "px";
+//         circle.className = "ripple animate";
+//         clickEl.appendChild(circle);
+//
+//     //clean up after animation completes remove div element
+//         ['animationend', 'webkitAnimationEnd', 'oAnimationEnd', 'MSAnimationEnd'].forEach(function (eventName) {
+//             circle.addEventListener(eventName, function () {
+//                 clickEl.removeChild(circle);
+//             });
+//         })
+//   });
 
-document.addEventListener('click', function (e) {
-    if (!e.target.matches('.ripple-effect')) {
-        return
-    }else {
-        //check to see if parent has ripple-effect
-        var clickEl = e.target.closest('.ripple-effect');
-        // if still null then element does not have ripple-effect
-        if (!clickEl) {
-            return
-        }
-    }
 
-        // we now have valid element with ripple-effect
-        // determine circle animation diameter
-        var rect = clickEl.getBoundingClientRect();
-        var x = event.clientX - rect.left;
-        var y = event.clientY - rect.top;
-        var circle = document.createElement("div");
 
-        circle.style.top = y + "px";
-        circle.style.left = x + "px";
-        circle.className = "ripple animate";
-        clickEl.appendChild(circle);
 
-    //clean up after animation completes remove div element
-        ['animationend', 'webkitAnimationEnd', 'oAnimationEnd', 'MSAnimationEnd'].forEach(function (eventName) {
-            circle.addEventListener(eventName, function () {
-                clickEl.removeChild(circle);
-            });
-        })
-  });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+///WORKING
 
 // document.addEventListener('click', function (event) {
 //     if (!event.target.classList.contains('ripple-effect')) {
